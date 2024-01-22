caa22dcb2691c4b31bd188e2fc5f9b60c390d01c
edselnombrado <edcel@bpoc.co.jp>
2024-01-22 18:01:24 +0800

    kulang responsive

--- front-page.php
+++ front-page.php
@@ -17,10 +17,66 @@
             <h1>section 1</h1>
         </div>
     </section>
+
     <section class="sect_2">
-        <div class="l-wrap">
-            <h1>section 2</h1>
-            <div>edsel</div>
+        <div class="second2-1 column-reverse">
+            <div class="datail-box">
+                <h2>自然循環栽培法</h2>
+                <p class="text1">Natural Circulation</p>
+                <p class="paragraph1">
+                    菌根菌と共存するぶどうの根は、地下深くからリン酸を吸収し、植物のエネルギー循環を活性化させ、糖の生産力を高めます。また、多種多様なミネラルやアミノ酸を吸収し、果実に蓄積することで、甘さと複雑さを増すと期待されています。一方、従来の農法では、地面から15cmの深さでのみ栄養を吸収し、化学肥料の使用により微生物の多様性が低下し、風味に影響を及ぼす可能性があります。
+                </p>
+                <a href="">
+                    詳しくはこちら
+                </a>
+            </div>
+            <div class="img-box">
+                <div>
+                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/section2/01.png" alt="">
+                </div>
+            </div>
+        </div>
+
+        <div class="second2-1">
+            <div class="img-box">
+                <div>
+                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/section2/02.png" alt="">
+                </div>
+            </div>
+            <div class="datail-box">
+                <h2>化学肥料は不使用</h2>
+                <p class="text1">No Chemical</p>
+                <p class="paragraph1">当園自慢のぶどうは、全て信州の環境にやさしい農産物50（長野県認証）認定を受けた、おいしさと安全を追求した逸品です。</p>
+
+                <h2 class="about-head">信州の環境にやさしい農産物について</h2>
+                <p class="paragraph1">
+                    化学肥料及び化学合成農薬について、「地域慣行施肥量」及び「地区農薬使用回数」の50％以上を削減(認証区分：50-50)した方法で栽培された農産物を認証する制度。詳しくはこちらをご覧ください。</p>
+                <div class="anchor-tag2">
+                    <a href="">
+                        長野県認証
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="second2-3">
+            <div class="l-wrap">
+                <div class="datail-box">
+                    <h2>生産者</h2>
+                    <p class="text1">Producer</p>
+                    <p class="paragraph1">『東御のぶどうの味をもっと沢山の人に伝えたい。』
+                        東京でITエンジニアとしてのキャリアから転身、ぶどう栽培の道へ進むため長野の東御に移住。自然循環栽培の草分け的な存在に。ぶどうの生食用にとどまらず、加工品も展開。東御ブランドの新しい可能性を探求し続けています。
+                    </p>
+                    <a href="">
+                        詳しくはこちら
+                    </a>
+                </div>
+                <div class="img-box">
+                    <img src="<?php echo get_template_directory_uri(); ?>/release/image/section2/03.png" alt="">
+                    <img class="signature"
+                        src="<?php echo get_template_directory_uri(); ?>/release/image/section2/04.png" alt="">
+                </div>
+            </div>
         </div>
     </section>
     <section class="sect_3">
